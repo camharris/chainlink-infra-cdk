fa137c8d3bf0c3190dccd4a855e78c0d287344a7
Cameron Harris <charris@planview.com>
2021-04-07 19:05:19 -0700

    Added kubernetes manifests for deployment and config map

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,11 +1,15 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2';
 import * as eks from '@aws-cdk/aws-eks';
+import * as secretsmanager from '@aws-cdk/aws-secretsmanager';
 import * as iam from '@aws-cdk/aws-iam';
+import { readYamlFromDir } from '../utils/readfile';
+import { HelmChart, KubernetesManifest } from '@aws-cdk/aws-eks';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
 
     // Build vpc with two public and private subnets
     const vpc = new ec2.Vpc(this, "Vpc", {
@@ -33,6 +37,7 @@ export class CdkStack extends cdk.Stack {
         },
       ]
     });
+    // TODO figure out how to tag resources
     //Tags.of(vpc).add("Name", "infra-kube-vpc");
 
 
@@ -51,7 +56,112 @@ export class CdkStack extends cdk.Stack {
       maxSize: 3,
     });
 
+    const namespace = cluster.addManifest('node-namespace', {
+      apiVersion: 'v1',
+      kind: 'Namespace',
+      metadata: { name: 'node-namespace'}
+    });
 
+    // Create secrets 
+    const postgresqlSecrets = new secretsmanager.Secret(this, 'postgresqlSecrets', {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ username: 'admin' }),
+        generateStringKey: 'password'
+      },
+    });
+
+    const nodeSecrets = new secretsmanager.Secret(this, 'nodeSecrets', {
+      generateSecretString: {
+        secretStringTemplate: JSON.stringify({ apiuser: 'charris@protonmail.ch' }),
+        generateStringKey: 'apipass',
+        //generateStringKey: ''
+      }
+    })
+
+    // TODO add wallet secret 
+
+    // Install postgres helm chart
+    new HelmChart(this, 'PostgreSQL', {
+    //const postgresChart = cluster.addHelmChart('PostgresSQL', {
+      cluster,
+      chart: 'postgresql',
+      repository: 'https://charts.bitnami.com/bitnami',
+      namespace: 'node-namespace', 
+      release: 'database',
+      values: {
+        'postgresqlPassword': postgresqlSecrets.secretValueFromJson('password'),
+        'postgresqlUsername': postgresqlSecrets.secretValueFromJson('username').toString(),
+        'postgresqlDatabase': 'chainlink'
+      },
+      
+    });        
+    //postgresChart.node.addDependency(namespace);
+
+
+    // Read Yaml files
+    //const manifestsFolder = 'manifests/';
+    //readYamlFromDir(manifestsFolder, cluster)
+  
+    const appLabel = { app: "chainlink-node" };
+
+    const node_config = {
+      apiVersion: "v1",
+      kind: "ConfigMap",
+      metadata: { name: "node-config" },
+      data: {
+        ROOT: "/chainlink",
+        LOG_LEVEL: "debug",
+        ETH_CHAIN_ID: 3,
+        MIN_OUTGOING_CONFIRMATIONS: 2,
+        LINK_CONTRACT_ADDRESS: "0x20fe562d797a42dcb3399062ae9546cd06f63280",
+        CHAINLINK_TLS_PORT: 0,
+        SECURE_COOKIES: false,
+        ORACLE_CONTRACT_ADDRESS: "0x9f37f5f695cc16bebb1b227502809ad0fb117e08",
+        ALLOW_ORIGINS: "*",
+        MINIMUM_CONTRACT_PAYMENT: 100,
+        DATABASE_URL: "postgresql://" + postgresqlSecrets.secretValueFromJson('username').toString() +
+                        postgresqlSecrets.secretValueFromJson('password') +
+                        "@postgres:5432/chainlink?sslmode=disable",
+        DATABASE_TIMEOUT: 0,
+        ETH_URL: "wss://ropsten-rpc.linkpool.io/ws",
+      },
+    };
+
+
+    new KubernetesManifest(this, 'nodeConfigMap', {
+      cluster,
+      manifest: [ node_config ]
+    });
+
+
+    const deployment = {
+      apiVersion: "apps/v1",
+      kind: "Deployment",
+      metadata: { name: "chainlink-node" },
+      spec: {
+        selector: { matchLabels: appLabel },
+        template: {
+          metadata: { labels: appLabel },
+          spec: {
+            containers: [
+            {
+              name: "chainlink-node",
+              image: "smartcontract/chainlink:0.10.3",
+              ports: [ { containerPort: 6688 } ]
+            }
+            ],
+            env_from: [{
+                configMapRef: { name: "node-config"}
+            }]
+          }
+        }
+      }
+    }; // End Deployment manifest
+
+    new KubernetesManifest(this, 'chainlink-deployment', {
+      cluster,
+      manifest: [ deployment ]
+    });
 
   }
 }
