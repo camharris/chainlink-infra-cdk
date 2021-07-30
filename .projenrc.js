da82c877314ce4975aadf8c33bc09bcf9415f78c
Cameron Harris <charris@planview.com>
2021-07-30 17:23:22 -0500

    inital commit

--- .projenrc.js
+++ .projenrc.js
@@ -1,15 +1,30 @@
 const { AwsCdkTypeScriptApp } = require('projen');
 const project = new AwsCdkTypeScriptApp({
-  cdkVersion: '1.95.2',
+  author: 'Cameron Harris',
+  authorAddress: 'charris@protonmail.ch',
+  cdkVersion: '1.116.0',
   defaultReleaseBranch: 'main',
-  name: 'cdk',
+  name: '@camharris/chainlink-infra-cdk',
 
-  // cdkDependencies: undefined,        /* Which AWS CDK modules (those that start with "@aws-cdk/") this app uses. */
-  // deps: [],                          /* Runtime dependencies of this module. */
-  // description: undefined,            /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],                       /* Build dependencies for this module. */
-  // packageName: undefined,            /* The "name" in package.json. */
-  // projectType: ProjectType.UNKNOWN,  /* Which type of project this is (library/app). */
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-ec2',
+    '@aws-cdk/aws-ecs',
+    '@aws-cdk/aws-ecr-assets',
+    '@aws-cdk/aws-ecs-patterns',
+    '@aws-cdk/aws-secretsmanager',
+    '@aws-cdk/aws-rds',
+  ],
+  description: 'A CDK application for deploying Chainlink oracle nodes', 
+  gitignore: ['cdk.out', 'cdk.context.json', '*notes*', '.env'],
+  packageName: 'chainlink-infra-cdk',            /* The "name" in package.json. */
+  entrypoint: 'cdk.ts',
+  tsconfig: {
+    compilerOptions: {
+      noUnusedLocals: false
+    }
+  },
+  // projectType: ProjectType.li,  /* Which type of project this is (library/app). */
   // release: undefined,                /* Add release management to this project. */
 });
 project.synth();
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,50 @@
-import { App, Construct, Stack, StackProps } from '@aws-cdk/core';
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { infraStack } from './lib/infra-stack';
+import { containerStack } from './lib/container-stack';
 
-export class MyStack extends Stack {
-  constructor(scope: Construct, id: string, props: StackProps = {}) {
-    super(scope, id, props);
+const app = new cdk.App();
 
-    // define resources here...
-  }
-}
+const myVpc = new infraStack(app, 'InfraStack', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+});
 
-// for development, use account/region from cdk cli
-const devEnv = {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-};
+const rinkebyNode = new containerStack(app, 'rinkeby-node', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  vpc: myVpc.vpc,
+  cluster: myVpc.cluster,
+  network: {
+    name:  "rinkeby",
+    eth_chain_id: "4",
+    eth_url: "wss://rinkeby.infura.io/ws/v3/c0d927dc916a4b85bdefbcfcd6204736",
+    min_outgoing_confirmations: "2",
+    link_contract_address: "0x01BE23585060835E02B77ef475b0Cc51aA1e0709"
+  },
+});
 
-const app = new App();
+const kovanNode = new containerStack(app, 'kovan-node', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  vpc: myVpc.vpc,
+  cluster: myVpc.cluster,
+  network: {
+    name:  "kovan",
+    eth_chain_id: "42",
+    eth_url: "wss://kovan.infura.io/ws/v3/c0d927dc916a4b85bdefbcfcd6204736",
+    min_outgoing_confirmations: "2",
+    link_contract_address: "0xa36085F69e2889c224210F603D836748e7dC0088"
+  },
+});
 
-new MyStack(app, 'my-stack-dev', { env: devEnv });
-// new MyStack(app, 'my-stack-prod', { env: prodEnv });
-
-app.synth();
+const container = new containerStack(app, 'mainnet-node', {
+  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+  vpc: myVpc.vpc,
+  cluster: myVpc.cluster,
+  network: {
+    name: "mainnet",
+    eth_chain_id: "1",
+    eth_url: "wss://mainnet.infura.io/ws/v3/c0d927dc916a4b85bdefbcfcd6204736",
+    min_outgoing_confirmations: "2",
+    link_contract_address: "0x514910771AF9Ca656af840dff83E8264EcF986CA"
+  },
+});
